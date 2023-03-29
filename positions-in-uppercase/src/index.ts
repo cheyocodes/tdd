cd0925a82bf77fbd2530af6c6e14026a24b4c63e
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-29 11:37:22 -0500

    extract uppercase validation into its own function

--- positions-in-uppercase/src/index.ts
+++ positions-in-uppercase/src/index.ts
@@ -1,8 +1,14 @@
+// Production code
+
+function isUpperCaseCharAt(index: number, word: string) {
+  return word[index] === word[index].toUpperCase();
+}
+
 export function getUpperCaseLetterPositions(word: string, i = 0): number[] {
   i = i || 0;
 
   if (word.length > i) {
-    if (word[i] == word[i].toUpperCase()) {
+    if (isUpperCaseCharAt(i, word)) {
       return [i].concat(getUpperCaseLetterPositions(word, i + 1));
     }
 
