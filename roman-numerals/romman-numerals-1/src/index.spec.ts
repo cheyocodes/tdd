5a83c98643f81aeeeffce7e1207865abb9c1da2e
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-30 11:15:17 -0500

    add roman-numeral tutorial final result

--- roman-numerals/romman-numerals-1/src/index.spec.ts
+++ roman-numerals/romman-numerals-1/src/index.spec.ts
@@ -1,7 +1,24 @@
 import { romanConverter } from './index';
 
-describe('test', () => {
-  it("should return '' when passing 0", async () => {
-    expect(romanConverter(0)).toBe('');
+describe('Roman Numerals', () => {
+  it.each([
+    ['', 0],
+    ['I', 1],
+    ['II', 2],
+    ['III', 3],
+    ['IV', 4],
+    ['V', 5],
+    ['VI', 6],
+    ['VII', 7],
+    ['VIII', 8],
+    ['IX', 9],
+    ['X', 10],
+    ['XII', 12],
+    ['XIV', 14],
+    ['XV', 15],
+    ['XIX', 19],
+    ['XXIII', 23],
+  ])("should return '%s' when passing %i", (expected: string, arg: number) => {
+    expect(romanConverter(arg)).toBe(expected);
   });
 });
--- roman-numerals/romman-numerals-1/src/index.ts
+++ roman-numerals/romman-numerals-1/src/index.ts
@@ -1,3 +1,23 @@
-export function romanConverter(number: number) {
-  return '';
+const symbols: Record<number, string> = {
+  0: '',
+  1: 'I',
+  4: 'IV',
+  5: 'V',
+  9: 'IX',
+  10: 'X',
+};
+
+const keys = Object.keys(symbols)
+  .map((key) => Number(key))
+  .reverse();
+
+const getKey = (number: number): number => {
+  return keys.filter((key) => number >= key)[0];
+};
+
+export function romanConverter(number: number): string {
+  const key = getKey(number);
+
+  if (key === 0) return symbols[0];
+  return symbols[key] + romanConverter(number - key);
 }
